d11fd0dcbafc2beb0f6330a8e0f3960cd4101ef2
SobuzjAlam <sobuzjalam@gmail.com>
2025-08-26 11:48:36 +0600

    # Exercise 01 -> Even or Odd

---
+++ controlFlow.py
@@ -0,0 +1,15 @@
+# basic control flow 
+
+"""
+Ternary operators -> 
+    Conditional expressions, It evaluates a condition and returns one of two values.
+    ***[value_if_true if condition else value_if_false]**
+"""
+
+# Exercise 01 -> Even or Odd 
+
+cheking_num = 7
+result = 'Even' if cheking_num % 2 == 0 else 'Odd'
+print(result) 
+
+
