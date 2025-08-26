3ca2e89ae6637b2775096ce0ee435f4c4aa07ee8
SobuzjAlam <sobuzjalam@gmail.com>
2025-08-26 12:19:19 +0600

    Exercise 01.1 List

--- controlFlow.py
+++ controlFlow.py
@@ -1,4 +1,4 @@
-# basic control flow 
+# basic control flow
 
 """
 Ternary operators -> 
@@ -6,10 +6,28 @@ Ternary operators ->
     ***[value_if_true if condition else value_if_false]**
 """
 
-# Exercise 01 -> Even or Odd 
+# Exercise 01 -> Even or Odd
 
 cheking_num = 7
-result = 'Even' if cheking_num % 2 == 0 else 'Odd'
-print(result) 
+result_01 = 'Even' if cheking_num % 2 == 0 else 'Odd'
+# print(result_01)
+
+#########################
+
+cheking_list = list(range(1, 101))
 
 
+def checkingOddEvenList(list):
+    odd_list = []
+    even_list = []
+    for i in list:
+        if ('Even' if i % 2 == 0 else 'Odd') == 'Odd':
+            odd_list.append(i)
+        else:
+            even_list.append(i)
+
+    return (f'ODD List: {odd_list} \nEVEN List: {even_list}')
+
+
+result_02 = checkingOddEvenList(cheking_list)
+# print(result_02)
