91cbfe3a9747c88b4d34153318424a183356ee37
SobuzjAlam <sobuzjalam@gmail.com>
2025-09-02 21:10:50 +0600

    Exercise

--- control_flow/controlFlow.py
+++ control_flow/controlFlow.py
@@ -87,3 +87,148 @@ def sum_natural_num(n):
 
 
 print(sum_natural_num(10))
+
+#########
+
+# Exercise 09 -> camelCase to snake_case.
+
+
+def main():
+    camelInput = input('camelCase: ')
+    snakeCase = convertCase(camelInput)
+    print(f'camelCase: {camelInput}\n snake_case: {snakeCase}')
+
+
+def convertCase(text):
+    caseText = []
+    for i, char in enumerate(text):
+        if char.isupper():
+            if i > 0:
+                caseText.append('_')
+            caseText.append(char.lower())
+        else:
+            caseText.append(char)
+    return ''.join(caseText)
+
+
+main()
+
+#########
+
+# Exercise 10 -> Coke insert coin.
+
+
+def main():
+    coke_due_price = 50
+    print(f'Amount Due: {coke_due_price}')
+    coin = 0
+    coin_denomination = [25, 10, 5]
+
+    while True:
+        insert_coin = int(input('Insert Coin: '))
+
+        if insert_coin in coin_denomination:
+            coin = insert_coin + coin
+            coke_due_price = coke_due_price - insert_coin
+
+        if coin >= 50:
+            print(f'Change Owed: {coin - 50}')
+            break
+
+        else:
+            print(f'Amount Due: {coke_due_price}')
+
+
+main()
+
+#########
+
+# Exercise 11 ->  insert emoji.
+
+
+def main():
+    data = input("")
+    text = findout(data)
+    print(text)
+
+
+def findout(face):
+    face = face.replace(":)", "🙂")
+    face = face.replace(":(", "🙁")
+    return face
+
+
+main()
+
+#########
+
+# Exercise 12 ->  Food Time.
+
+
+def main():
+    time = input('What time is it? ')
+    given_time = convert(time)
+    if 7 <= given_time <= 8:
+        print('breakfast time')
+    elif 12 <= given_time <= 13:
+        print('lunch time')
+    elif 18 <= given_time <= 19:
+        print('dinner time')
+
+
+def convert(time):
+    hours, minutes = map(int, time.split(':'))
+    return hours + minutes / 60
+
+#########
+
+# Exercise 13 ->  Remove vowel from words.
+
+
+def main():
+    user_text = input('Input: ')
+    without_vowels = check_vowels(user_text)
+    print(f'Output: {without_vowels}')
+
+
+def check_vowels(text):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    checked_vowels = []
+    for chr in text:
+        if chr.lower() in vowels:
+            checked_vowels.append('')
+        else:
+            checked_vowels.append(chr)
+
+    return ''.join(checked_vowels)
+
+
+main()
+
+#########
+
+# Exercise 14 ->  cs50 plate.
+
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    find_zero = s[2:].find('0')
+    if 6 >= len(s) >= 2 and s[0:2].isalpha() and s.isalnum() and find_zero != 0:
+        for n in s[2:]:
+            if n.isdigit():
+                i = s.index(n)
+                if s[i:].isdigit():
+                    return True
+                else:
+                    return False
+        return True
+
+
+main()
--- main.py
+++ main.py
@@ -1,4 +1,1 @@
-# from control_flow.ternary import *
-from control_flow.controlFlow import *
 
-# print(result)
